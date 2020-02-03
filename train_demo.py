9d5c7e008eb9985b071ef4cd8a384ba4949b756d
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-02-03 10:55:16 -0500

    add learning rate warm up, calculate mAP every x epochs

--- train_demo.py
+++ train_demo.py
@@ -11,10 +11,12 @@ import matplotlib.pyplot as plt
 import timeit
 import random
 
+from datetime import datetime
 from datasets.kitti import KITTI
 from pixor_utils.model_utils import load_model, save_model
 from pixor_utils.training_gen import TrainingGenerator
 from pixor_utils.prediction_gen import PredictionGenerator
+from get_pred import generate_preds
 
 from test_utils.unittest import test_pc_encoder, test_target_encoder
 
@@ -26,6 +28,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = configs["gpu_id"]
 np.random.seed(0)
 
 import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 
 device_name = tf.test.gpu_device_name()
 os.system('clear')
@@ -52,9 +56,10 @@ MAX_Q_SIZE = configs['hyperparams']['max_q_size']
 
 # Create dirs
 OUTPUTS_DIR = os.path.join('outputs')
-CKPTS_DIR = os.path.join(OUTPUTS_DIR, configs['ckpts_dir'])
+CKPTS_DIR   = os.path.join(OUTPUTS_DIR, configs['ckpts_dir'])
+# LOG_DIR     = os.path.join(CKPTS_DIR, configs['experiment_name'][1:] + datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
 
-for d in [OUTPUTS_DIR, CKPTS_DIR]:
+for d in [OUTPUTS_DIR, CKPTS_DIR]:#, LOG_DIR]:
     print("Creating directory: " + d)
     os.makedirs(d, exist_ok=True)
 
@@ -69,6 +74,19 @@ target_encoder = configs['target_encoder'](shape=TARGET_SHAPE,
                                             target_means=TARGET_MEANS, target_stds=TARGET_STDS,
                                             mean_height=MEAN_HEIGHT, mean_altitude=MEAN_ALTITUDE,
                                             P_WIDTH=P_WIDTH, P_HEIGHT=P_HEIGHT, P_DEPTH=P_DEPTH)
+
+# rand_idx = np.random.randint(0, len(train_ids))
+# test_id  = train_ids[rand_idx]
+# img = kitti.get_image(test_id)
+# plt.imshow(img)
+# plt.axis('off')
+# plt.savefig('1.png', bbox_inches='tight', pad_inches=0)
+# print(img.shape)
+# img = tf.image.resize(img, (img.shape[0] // 4, img.shape[1] // 4))
+# print(img.shape)
+# plt.imshow(img)
+# plt.axis('off')
+# plt.savefig('2.png', bbox_inches='tight', pad_inches=0)
 
 #--------------------------------------#
 #-----------RUN UNIT TESTS-------------#
@@ -89,9 +107,11 @@ else:
 optimizer = configs['optimizer'](lr=LEARNING_RATE)
 losses = configs['losses']
 metrics = configs['metrics']
+
+
 model.compile(optimizer=optimizer,
-                      loss=losses,
-                      metrics=metrics)
+              loss=losses,
+              metrics=metrics)
 
 model.summary()
 
@@ -102,14 +122,35 @@ print('Model create/loaded [OK]')
 print('Model compiled')
 print('Training will start... NOW!')
 
+os.system('cp {0} {1}'.format(configs['current_file_name'], CKPTS_DIR + '/'))
+
 experiment = CometMLLogger()
 
-val_steps = 0
+val_steps   = 0
 train_steps = 0
+lr_steps    = 0
+cur_lr = LEARNING_RATE
 
-for cur_epoch in range(EPOCHS):
+def calc_lr(epoch, steps_per_epoch, min_lr, max_lr, cycle_length):
+    pass
+
+def schedule(epoch, lr):
+    if epoch % 20 == 0:
+        if lr == 0.001:
+            lr = 0.01
+        else:
+            lr = 0.001
+    return lr
+    # if epoch < 20:
+    #     return 0.001
+    # elif epoch < 40:
+    #     return 0.01
+    # else:
+    #     return 0.0001
+
+for cur_epoch in range(1, EPOCHS):
     with experiment.experiment.train():
-        train_gen = TrainingGenerator(reader=kitti, frame_ids=train_ids[:100], batch_size=BATCH_SIZE,
+        train_gen = TrainingGenerator(reader=kitti, frame_ids=train_ids, batch_size=BATCH_SIZE,
                                       pc_encoder=pc_encoder, target_encoder=target_encoder, 
                                       n_threads=NUM_THREADS, max_queue_size=MAX_Q_SIZE)
 
@@ -120,17 +161,32 @@ for cur_epoch in range(EPOCHS):
         cur_progress = progress
 
         print('Total number of batches is ->',train_gen.batch_count)
-
+        
         for batch_id in range(train_gen.batch_count):
             # Fetch batch
             batch = train_gen.get_batch()
             encoded_pcs, encoded_targets = batch['encoded_pcs'], batch['encoded_targets']
+            
+            # K.set_value(model.optimizer.lr, calc_lr(epoch=cur_epoch, steps_per_epoch=10, min_lr=1e-4, max_lr=1e-2, cycle_length=30))
+            # K.set_value(model.optimizer.lr, schedule(cur_epoch, model.optimizer.lr.numpy()))
+            # print('in epoch ->', cur_epoch, 'cur lr ->', model.optimizer.lr.numpy())
+            if train_steps is not 0 and train_steps % (len(train_ids) // 4) == 0:
+                if cur_lr == 0.001:
+                    print('changing to 0.0001')
+                    cur_lr = 0.0001
+                else:
+                    print('changing to 0.001')
+                    cur_lr = 0.001
+                if cur_epoch > 5:
+                    cur_lr = 0.0001
+            K.set_value(model.optimizer.lr, cur_lr)
+            # print('cur epoch ->', cur_epoch, ' cur lr ->', model.optimizer.lr.numpy())
+            metrics = model.train_on_batch(x=encoded_pcs, y=encoded_targets)
+            
+            for metric_name, metric_val in zip(model.metrics_names, metrics):
+                experiment.log_metric(metric_name, metric_val, train_steps)
 
-            total_loss, geo_loss, obj_loss = model.train_on_batch(x=encoded_pcs, y=encoded_targets)
-
-            experiment.log_metric('total_loss', total_loss, train_steps)
-            experiment.log_metric('obj_loss', obj_loss, train_steps)
-            experiment.log_metric('geo_loss', geo_loss, train_steps)
+            experiment.log_metric('lr', cur_lr, train_steps)
 
             train_steps += 1
 
@@ -145,6 +201,14 @@ for cur_epoch in range(EPOCHS):
         os.system('clear')
         print('Finished training the {0}-th epoch in {1}'.format(cur_epoch + 1 + configs['start_epoch'], (timeit.default_timer() - start) // 60))
         
+    if cur_epoch % configs['hyperparams']['map_every'] == 0 and cur_epoch is not 0:
+        generate_preds(model=model, kitti_reader=kitti, pc_encoder=pc_encoder, 
+                       target_encoder=target_encoder, frame_ids=val_ids,
+                       epoch=cur_epoch, ckpts_dir=CKPTS_DIR, exp_id=configs['experiment_name'][1:-1])
+        os.system('clear')
+        print('Got mAPs for you!')
+        print('-------------------------------------------')
+    
     if cur_epoch % configs['hyperparams']['validate_every'] == 0 and cur_epoch is not 0:
         with experiment.experiment.validate():
             val_samples = configs['hyperparams']['n_val_samples']
