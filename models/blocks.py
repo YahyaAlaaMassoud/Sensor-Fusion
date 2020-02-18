98dbef5403554c6707c2b1269706e163593d4fdb
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-02-18 13:52:31 -0500

    add name scope to conv_block

--- models/blocks.py
+++ models/blocks.py
@@ -1,20 +1,24 @@
 
+import tensorflow as tf
+
 from tensorflow.keras.layers import Conv2D, SeparableConv2D, ReLU, BatchNormalization, Add
 
 def create_conv_block(input_tensor, filters, kernel_size, num_layers):
   x = input_tensor
-  for _ in range(num_layers):
-    x = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', use_bias=False)(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
+  for i in range(num_layers):
+    with tf.name_scope("ConvBlock"):
+      x = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', use_bias=False)(x)
+      x = BatchNormalization()(x)
+      x = ReLU()(x)
   return x
 
 def create_sep_conv_block(input_tensor, filters, kernel_size, num_layers):
   x = input_tensor
   for _ in range(num_layers):
-    x = SeparableConv2D(filters=filters, kernel_size=kernel_size, padding='same', use_bias=False)(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
+    with tf.name_scope("SepConvBlock"):
+      x = SeparableConv2D(filters=filters, kernel_size=kernel_size, padding='same', use_bias=False)(x)
+      x = BatchNormalization()(x)
+      x = ReLU()(x)
   return x 
 
 def create_res_conv_block(input_tensor, filters, kernel_size, num_layers):
