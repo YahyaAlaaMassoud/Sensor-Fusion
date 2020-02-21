7b6a039addff8fe004cfaf0d6ee8b84296a7cbe5
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-02-20 19:56:53 -0500

    further updates to the add random sample augmentation code

--- add_random_sample.py
+++ add_random_sample.py
@@ -9,6 +9,7 @@ from core.boxes import Box3D
 from data_utils.augmentation import PointCloudAugmenter
 from pprint import pprint
 from operator import itemgetter
+from shapely.geometry import Polygon
 
 '''
     https://stackoverflow.com/questions/21037241/how-to-determine-a-point-is-inside-or-outside-a-cube
@@ -17,12 +18,23 @@ from operator import itemgetter
     https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
     https://www.cs.hmc.edu/ACM/lectures/intersections.html
     https://docs.yoyogames.com/source/dadiospice/002_reference/movement%20and%20collisions/collisions/rectangle_in_triangle.html
+    https://math.stackexchange.com/questions/3210769/how-do-i-check-if-a-3d-point-is-between-2-other-3d-points
 '''
 
 DS_DIR = '/home/salam/datasets/KITTI/training'
 reader = KITTI(DS_DIR, CARS_ONLY)
 
 ids = reader.get_ids('train')
+
+def check_colinear(a, b, c):
+    ab = b - a
+    ac = c - a
+    cross = np.cross(ab, ac)
+    if cross.all() < 1e-12:
+        # print(cross)
+        # print(a, b, c)
+        return True
+    return False
 
 def get_angle_between_two_vectors(src, p2, p3):
     a = src - p2
@@ -105,7 +117,18 @@ def point_in_triangle(p, p0, p1, p2):
         return s <= 0 and s + t >= A
     return s >= 0 and s + t <= A
 
-t = '000010'#ids[4]
+def is_box_valid(limit, box_bev_pts):
+    (x1, y1), (x2, y2) = limit
+    limit_p = Polygon([(0, 0), (x1, y1), (x2, y2)])
+    box_p   = Polygon([(box_bev_pts[0][0], box_bev_pts[0][1]),
+                        (box_bev_pts[1][0], box_bev_pts[1][1]),
+                        (box_bev_pts[2][0], box_bev_pts[2][1]),
+                        (box_bev_pts[3][0], box_bev_pts[3][1])])
+    if limit_p.intersection(box_p).area > 0:
+        return False
+    return True
+
+t = '000013'#ids[4]
 boxes = reader.get_boxes_3D(t)
 pts, ref = reader.get_velo(t, use_fov_filter=False)
 
@@ -121,10 +144,10 @@ circles = []
 d1_clr, d2_clr = (255, 0, 0), (0, 255, 0)
 box_limits = []
 all_limits = []
-
+rand_num   = 30
 random_cars_dir = 'data_utils/aug_utils/annotations/cars/'
 random_cars = os.listdir(random_cars_dir)
-random_cars = np.random.choice(random_cars, size=20)
+random_cars = np.random.choice(random_cars, size=rand_num)
 
 sampled_cars = []
 for random_car in random_cars:
@@ -147,9 +170,9 @@ for random_car in random_cars:
     }
     sampled_cars.append(data)
     
-sampled_cars = sorted(sampled_cars, key=itemgetter('num_pts'), reverse=True)
+sampled_cars = sorted(sampled_cars, key=itemgetter('num_pts'), reverse=False)
 
-def get_box_limit(box, name=None):
+def get_box_limit(box):
     diag_1, diag_2 = box.get_bev_diags()
     angle_diag_1 = get_angle_between_two_vectors(lidar_src, diag_1[0], diag_1[1])
     angle_diag_2 = get_angle_between_two_vectors(lidar_src, diag_2[0], diag_2[1])
@@ -164,15 +187,8 @@ def get_box_limit(box, name=None):
     # c = box.get_bev_center()
     # circles.append((c, d1_clr))
     
-    # print((ref_diag[0][0], ref_diag[0][1]))
-    # print((ref_diag[1][0], ref_diag[1][1]))
     inter1_x, inter1_y, _, _, _ = intersectLines(border_1[0], border_1[1], lidar_src, (ref_diag[0][0], ref_diag[0][1]))
     inter2_x, inter2_y, _, _, _ = intersectLines(border_1[0], border_1[1], lidar_src, (ref_diag[1][0], ref_diag[1][1]))
-    # print(inter1_x, inter1_y)
-    # print(inter2_x, inter2_y)
-    
-    if name:
-        print(name)
         
     all_limits.append(np.array([[inter1_y, 0, inter1_x],
                        [0,0,0]]))
@@ -183,41 +199,60 @@ def get_box_limit(box, name=None):
     return (inter1_x, inter1_y), (inter2_x, inter2_y)
 
 for i, box in enumerate(boxes):
-    
     box = boxes[i]
-    
     (x1, y1), (x2, y2) = get_box_limit(box)
-    
     box_limits.append(((x1, y1), (x2, y2)))
+    
 
-cnt = 0
+cnt     = 0
+new_pts = []
 for car in sampled_cars:
     valid = True
     for limit in box_limits:
         (x1, y1), (x2, y2) = limit
-        limit = np.array([[x1, y1],
-                          [x2, y2]])
-        for pt in car['bev_corners']:
-            is_found = point_in_triangle(pt, lidar_src, limit[0], limit[1])
-            if is_found:
-                valid = False
-        if point_in_triangle(car['bev_center'], lidar_src, limit[0], limit[1]):
+        
+        box_pts = car['bev_corners']
+        
+        limit_p = Polygon([(0,0), (x1, y1), (x2, y2)])
+        box_p   = Polygon([(box_pts[0][0], box_pts[0][1]),
+                           (box_pts[1][0], box_pts[1][1]),
+                           (box_pts[2][0], box_pts[2][1]),
+                           (box_pts[3][0], box_pts[3][1])])
+        if limit_p.intersection(box_p).area > 0:
             valid = False
+        # print('res', valid)
+        # print('---')
+        del limit_p, box_p
     
     if valid:
-        pts = np.concatenate((pts, car['pts']), axis=1)
+        new_pts.append(car['pts'])
         boxes.append(car['box'])
-        print(len(all_limits))
-        (x1, y1), (x2, y2) = get_box_limit(box, 'new')
-        print(len(all_limits))
+        (x1, y1), (x2, y2) = get_box_limit(car['box'])
         box_limits.append(((x1, y1), (x2, y2)))
     else:
         cnt += 1
 
-print('la2 {0}'.format(cnt))
-# print(len(all_limits))
+print('sampled {0} out of {1}'.format(rand_num-cnt, rand_num))
 
 
-open3d(pts, boxes, limits=all_limits)
+ss = None
+for new_sample in new_pts:
+    new_sample_pts = new_sample.T
+    # pts = np.concatenate((pts, new_sample), axis=1)
+    print(new_sample_pts.shape)
+    new_ids = []
+    for i in range(new_sample_pts.shape[0]):
+        q = pts.T - new_sample_pts[i,:]
+        s = np.reshape(-1. * new_sample_pts[i,:], (1,3))
+        # print(q.shape, s.shape)
+        cc = np.cross(s, q)
+        # print(cc.shape)
+        inds = np.where(cc == 0)[0]
+        # print(len(inds), inds)
+        new_ids.extend(inds)
+print(len(new_ids))
+ss = pts.T[new_ids]
+
+open3d(ss.T, boxes, limits=all_limits)
 # # open3d(sampled_pts2, boxes)
 # imshow(bev(pts, pred_boxes=boxes, title="GT", circles=circles))
