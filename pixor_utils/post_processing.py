41f4b7e389e327f2aa8af5a67676e2ed359c03ea
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-02-25 10:02:26 -0500

    add distance nms + remove np.delete

--- pixor_utils/post_processing.py
+++ pixor_utils/post_processing.py
@@ -29,7 +29,7 @@ def iou_bev(axis_aligned=False):
     return compute_iou_bev
 
 def dist_bev(box_1, box_2):
-    return np.sqrt((box_1.x - box_2.x) ** 2 + (box_1.z - box_2.z) ** 2)
+    return - np.sqrt((box_1.x - box_2.x) ** 2 + (box_1.z - box_2.z) ** 2)
 
 def nms_bev(nms_type, thresh, max_boxes=100, min_hit=0, axis_aligned=False):
     if nms_type not in ['iou', 'dist']:
@@ -42,8 +42,8 @@ def nms_bev(nms_type, thresh, max_boxes=100, min_hit=0, axis_aligned=False):
 
     def nms(boxes):
         boxes.sort(key=lambda box: box.confidence, reverse=True)
-        if len(boxes) > 500:
-            boxes = boxes[:500]
+        if len(boxes) > 800:
+            boxes = boxes[:800]
         filtered_boxes = []
 
         while len(boxes) > 0 and len(filtered_boxes) < max_boxes:
