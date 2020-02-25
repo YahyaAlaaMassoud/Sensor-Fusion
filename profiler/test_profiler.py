f132fe663e20ad16f5a5604dad20b3ee85ac5753
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-02-25 11:52:34 -0500

    adding profiling coding (computing FLOPS/MACs

--- profiler/test_profiler.py
+++ profiler/test_profiler.py
@@ -1,46 +1,26 @@
+
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
 from pixor_utils.model_utils import load_model
-from training_utils.losses import total_loss
-from training_utils.metrics import objectness_metric, regression_metric
-from models.pixor_det import BiFPN
-from tensorflow.keras.optimizers import Adam
-
-
+from models.pixor_det import BiFPN, create_pixor_det
+from models.pixor_pp import create_pixor_pp
 
 run_meta = tf.RunMetadata()
 
 with tf.Session(graph=tf.Graph()) as sess:
     K.set_session(sess)
-    sess.run(tf.global_variables_initializer())
-    model = load_model('outputs/car_3bifpn_concat_aug50random_abs_densify/car_3bifpn_concat_aug50random_abs_densify_epoch_1.json', 
-                       'outputs/car_3bifpn_concat_aug50random_abs_densify/car_3bifpn_concat_aug50random_abs_densify_epoch_1.h5', 
+#     sess.run(tf.global_variables_initializer())
+#     model = create_pixor_pp()
+    net = load_model('outputs/ckpts_3bifpn_car_aug_abs_3d/pixor_bifpn_car_noRelu_conv_aug_abs__epoch_8.json', 
+                       'outputs/ckpts_3bifpn_car_aug_abs_3d/pixor_bifpn_car_noRelu_conv_aug_abs__epoch_8.h5', 
                        {'BiFPN': BiFPN})
 
-    optimizer = Adam(lr=0.0001)
-    losses = {
-            'output_map': total_loss(alpha=0.25, gamma=2.0, reg_loss_name='abs', reg_channels=8, weight=1.0, subsampling_flag=False)
-            }
-    metrics = {
-            'output_map': [objectness_metric(alpha=0.25, gamma=2.0, subsampling_flag=False),
-                            regression_metric(reg_loss_name='abs', reg_channels=8, weight=1.0, subsampling_flag=False)],
-            }
 
-    for layer in model.layers:
-        layer.trainable = False
-
-    model.compile(optimizer=optimizer,
-                    loss=losses,
-                    metrics=metrics)
-
-    # model.summary()
-    net = model(inputs=tf.compat.v1.placeholder('float32', shape=(1,800,700,35)))
+#     net.summary()
 
     opts = tf.profiler.ProfileOptionBuilder.float_operation()    
     flops = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
 
-    # opts = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()    
-    # params = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
-
-    # print("{:,} --- {:,}".format(flops.total_float_ops, params.total_parameters))
+    opts = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()    
+    params = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
