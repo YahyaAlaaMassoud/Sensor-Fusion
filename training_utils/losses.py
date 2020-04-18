f0c6316d7227905d201ebb5346305634291a8152
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-04-18 17:11:24 -0400

    update loss & metrics

--- training_utils/losses.py
+++ training_utils/losses.py
@@ -2,110 +2,96 @@
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
-_EPSILON = tf.keras.backend.epsilon()
+import matplotlib.pyplot as plt
+import numpy as np
 
-def focal_loss(alpha, gamma, subsampling_flag):
-    def focal_loss_fixed(y_true, y_pred):
-        y_true_cls       = y_true[:, :, :, 0]
+
+def focal(alpha, gamma, subsampling_flag, data_format='channels_last'):
+
+    def _focal(y_true, y_pred):
         subsampling_mask = 1.0
         if subsampling_flag:
-            subsampling_mask = y_true[:, :, :, 1]
-            
-        pos_mask = tf.cast(tf.equal(y_true_cls, 1.0), tf.float32)
-        neg_mask = tf.cast(tf.less(y_true_cls, 1.0), tf.float32)
+            subsampling_mask = y_true[...,1:2]
 
-        pos_loss = -tf.math.log(tf.clip_by_value(y_pred, _EPSILON, 1.0 - _EPSILON)) * tf.pow(1 - y_pred, gamma) * pos_mask
-        neg_loss = -tf.math.log(tf.clip_by_value(1 - y_pred, _EPSILON, 1.0 - _EPSILON)) * tf.pow(y_pred, gamma) * neg_mask
+        y_true = y_true[...,0:1]
+        ce = K.binary_crossentropy(y_true, y_pred, from_logits=False)
 
-        num_pos = tf.reduce_sum(pos_mask)
-        pos_loss = tf.reduce_sum(pos_loss) * alpha
-        neg_loss = tf.reduce_sum(neg_loss) * (1 - alpha)
+        alpha_factor = K.ones_like(y_true) * alpha
+        alpha_factor = tf.where(K.equal(y_true, 1), alpha_factor, 1 - alpha_factor)
 
-        return (pos_loss + neg_loss) / (num_pos + 1)
+        focal_weight = tf.where(K.equal(y_true, 1), 1 - y_pred, y_pred)
+        focal_weight = alpha_factor * (focal_weight ** gamma)
 
-    return focal_loss_fixed
+        obj_loss = focal_weight * ce * subsampling_mask
+        obj_loss_sum = tf.reduce_sum(obj_loss)
 
+        num_pos = tf.reduce_sum(y_true) + 1.0
 
-def binary_focal_loss(alpha, gamma, subsampling_flag):
+        return obj_loss_sum / num_pos
+        # return tf.reduce_sum(ce)/ num_pos
     
-    def compute_binary_focal_loss(y_true, y_pred):
-        y_true_cls       = y_true[:, :, :, 0]
-        subsampling_mask = 1.0
-        if subsampling_flag:
-            subsampling_mask = y_true[:, :, :, 1]
-        
-        y_pred = tf.clip_by_value(y_pred, _EPSILON, 1.0 - _EPSILON)
+    _focal.__name__ = 'focal_loss'
 
-        ce = K.binary_crossentropy(y_true_cls, y_pred, from_logits=False)
-        
-        y_true_comp = (1.0 - y_true_cls)
-        y_pred_comp = tf.clip_by_value(1.0 - y_pred, _EPSILON, 1.0 - _EPSILON)
-        
-        p_t = (y_true_cls * y_pred) + (y_true_comp * y_pred_comp)
-        
-        alpha_factor = (y_true_cls * alpha + y_true_comp * (1.0 - alpha))
-        
-        modulating_factor = tf.pow((1.0 - p_t), gamma)
+    return _focal
 
-        num_pos = tf.reduce_sum(y_true_cls) + 1.0
-        loss = alpha_factor * modulating_factor * ce * subsampling_mask
-        loss = tf.where(tf.math.is_nan(loss), tf.zeros_like(loss), loss)
 
-        return tf.reduce_sum(loss) / num_pos
+def abs_loss(reg_channels, data_format='channels_last'):
 
-    return compute_binary_focal_loss
+    def compute_abs_loss(y_true, y_pred):
 
-@tf.function(experimental_relax_shapes=True)
-def absolute_diff_loss(y_true, y_pred):
-    abs_diff = tf.abs(y_true - y_pred)
-    loss = tf.reduce_mean(abs_diff)
-    if tf.math.is_nan(loss):
-        return abs_diff
-    return loss
+        mask = tf.reshape(tf.equal(y_true[...,-1], 1.0), shape=(-1,))
+        pos_count = tf.reduce_sum(tf.cast(mask, tf.float32))
 
-def smooth_l1_loss(weight):
+        y_true = y_true[...,:-1]
+        y_true_masked = tf.boolean_mask(tf.reshape(y_true, shape=(-1, reg_channels)), 
+                                        mask=mask, 
+                                        axis=0)
+        y_pred_masked = tf.boolean_mask(tf.reshape(y_pred, shape=(-1, reg_channels)),
+                                        mask=mask,
+                                        axis=0)
+        if y_true_masked.shape[0] == 0:
+            return tf.constant(value=0., dtype=tf.float32)
+
+        abs_diff  = tf.abs(y_true - y_pred)
+        sum = tf.reduce_sum(abs_diff, axis=1)
+        loss = tf.reduce_mean(sum)
+        return loss
+    
+    compute_abs_loss.__name__ = 'absolute_diff'
+
+    return compute_abs_loss
+
+
+
+def smooth_l1_loss(sigma, reg_channels, data_format='channels_last'):
+
+    sigma_squared = sigma ** 2
 
     def compute_smooth_l1_loss(y_true, y_pred):
+
+        mask = tf.reshape(tf.equal(y_true[...,-1], 1.0), shape=(-1,))
+        pos_count = tf.reduce_sum(tf.cast(mask, tf.float32))
+
+        y_true = y_true[...,:-1]
+        y_true_masked = tf.boolean_mask(tf.reshape(y_true, shape=(-1, reg_channels)), 
+                                        mask=mask, 
+                                        axis=0)
+        y_pred_masked = tf.boolean_mask(tf.reshape(y_pred, shape=(-1, reg_channels)),
+                                        mask=mask,
+                                        axis=0)
+        if y_true_masked.shape[0] == 0:
+            return tf.constant(value=0., dtype=tf.float32)
+
         abs_diff  = tf.abs(y_true - y_pred)
-        smooth_l1 = tf.where(abs_diff < 1, 0.5 * tf.square(abs_diff), abs_diff - 0.5)
-        return smooth_l1
+
+        smooth_l1 = tf.where(abs_diff < (1. / sigma_squared), 
+                             0.5 * sigma_squared * tf.square(abs_diff), 
+                             abs_diff - 0.5 / sigma_squared)
+
+        sum = tf.reduce_sum(smooth_l1, axis=1)
+        loss = tf.reduce_mean(sum)
+        return loss
+    
+    compute_smooth_l1_loss.__name__ = 'smooth_l1'
 
     return compute_smooth_l1_loss
-
-def total_loss(alpha, gamma, reg_loss_name, reg_channels, weight, subsampling_flag):
-    '''
-        erosion mask is expected to be the second channel in the output
-    '''
-    obj_loss_fn = focal_loss(alpha, gamma, subsampling_flag)
-    
-    reg_loss_fn = smooth_l1_loss(weight)
-    if reg_loss_name == 'abs':
-        reg_loss_fn = absolute_diff_loss
-    
-    def compute_total_loss(y_true, y_pred):
-        reg_start = 1
-        if subsampling_flag:
-            reg_start = 2
-
-        y_true_cls, y_true_reg = y_true[:, :, :, :reg_start], y_true[:, :, :, reg_start:]
-        y_pred_cls, y_pred_reg = y_pred[:, :, :, 0], y_pred[:, :, :, 1:]
-
-        # Objectness Loss
-        obj_loss = obj_loss_fn(y_true_cls, y_pred_cls)
-
-        # Regression Loss
-        mask = tf.reshape(tf.equal(y_true_cls[:,:,:,0], 1.0), shape=(-1,))
-        # print('mask', mask.shape)
-        y_true_reg_masked = tf.boolean_mask(tf.reshape(y_true_reg, shape=(-1, reg_channels)), 
-                                            mask=mask, 
-                                            axis=0)
-        # print('gt', y_true_reg_masked.shape)
-        y_pred_reg_masked = tf.boolean_mask(tf.reshape(y_pred_reg, shape=(-1, reg_channels)),
-                                            mask=mask,
-                                            axis=0)
-        # print('pred', y_pred_reg_masked.shape)
-        reg_loss = reg_loss_fn(y_true_reg_masked, y_pred_reg_masked)
-
-        return obj_loss + reg_loss
-    
-    return compute_total_loss
--- training_utils/metrics.py
+++ training_utils/metrics.py
@@ -2,42 +2,45 @@
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
-from .losses import binary_focal_loss, smooth_l1_loss, absolute_diff_loss, focal_loss
+def reg_metric(value_idx, reg_channels):
 
-def objectness_metric(alpha, gamma, subsampling_flag):
-    
-    obj_loss_fn = focal_loss(alpha, gamma, subsampling_flag)
-    
-    def compute_objectness_metric(y_true, y_pred):
-        metric_value = obj_loss_fn(y_true, y_pred[:, :, :, 0])
-        return metric_value
-    
-    return compute_objectness_metric
+    def compute_metric(y_true, y_pred):
+        mask = tf.reshape(tf.equal(y_true[...,-1], 1.0), shape=(-1,))
+        pos_count = tf.reduce_sum(tf.cast(mask, tf.float32))
 
-def regression_metric(reg_loss_name, reg_channels, weight, subsampling_flag):
+        y_true = y_true[:,:,:,:-1]
+        y_true_masked = tf.boolean_mask(tf.reshape(y_true, shape=(-1, reg_channels)), 
+                                        mask=mask, 
+                                        axis=0)
+        y_pred_masked = tf.boolean_mask(tf.reshape(y_pred, shape=(-1, reg_channels)),
+                                        mask=mask,
+                                        axis=0)
+
+        y_true = y_true_masked[:,value_idx]
+        y_pred = y_pred_masked[:,value_idx]
+
+        return tf.cond(tf.equal(tf.size(y_true), 0), lambda: tf.constant(0.0), lambda: tf.reduce_mean(tf.abs(y_true - y_pred))) 
     
-    reg_loss_fn = smooth_l1_loss(weight)
-    if reg_loss_name == 'abs':
-        reg_loss_fn = absolute_diff_loss
-    
-    def compute_regression_metric(y_true, y_pred):
-        reg_start = 1
-        if subsampling_flag:
-            reg_start = 2
-            
-        y_true_cls, y_true_reg = y_true[:, :, :, 0], y_true[:, :, :, reg_start:]
-        _         , y_pred_reg = y_pred[:, :, :, 0], y_pred[:, :, :, 1:]
-
-        # Regression Loss
-        mask = tf.reshape(tf.equal(y_true_cls, 1.0), shape=(-1,))
-        y_true_reg_masked = tf.boolean_mask(tf.reshape(y_true_reg, shape=(-1, reg_channels)), 
-                                            mask=mask, 
-                                            axis=0)
-        y_pred_reg_masked = tf.boolean_mask(tf.reshape(y_pred_reg, shape=(-1, reg_channels)),
-                                            mask=mask,
-                                            axis=0)
-        metric_value = reg_loss_fn(y_true_reg_masked, y_pred_reg_masked)
-
-        return metric_value
-
-    return compute_regression_metric
+    if value_idx == 0:
+        compute_metric.__name__ = 'cos_theta'
+    elif value_idx == 1:
+        compute_metric.__name__ = 'sin_theta'
+    elif value_idx == 2:
+        compute_metric.__name__ = 'z_offset'
+    elif value_idx == 3:
+        compute_metric.__name__ = 'x_offset'
+    elif value_idx == 4:
+        compute_metric.__name__ = 'altitude'
+    elif value_idx == 5:
+        compute_metric.__name__ = 'log_w'
+    elif value_idx == 6:
+        compute_metric.__name__ = 'log_l'
+    elif value_idx == 7:
+        compute_metric.__name__ = 'log_h'
+    elif value_idx == 8:
+        compute_metric.__name__ = 'ratio_log_w'
+    elif value_idx == 9:
+        compute_metric.__name__ = 'ratio_log_l'
+    elif value_idx == 10:
+        compute_metric.__name__ = 'ratio_log_h'
+    return compute_metric
