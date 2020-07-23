e5198db716137427812a99b74057f572ad1af670
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-07-23 02:13:41 -0400

    - adapt input size to the size of the new voxelizer output - modify BEV backbone - fix FPN implementation

--- models/sensor_fusion_net.py
+++ models/sensor_fusion_net.py
@@ -5,36 +5,31 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import UpSampling2D, Conv2DTranspose, AveragePooling2D, ZeroPadding2D, Add, Conv2D, Flatten, Reshape, MaxPooling2D, Average, Concatenate
 from pprint import pprint
 
-from models.blocks import create_res_conv_block, create_input_layer, conv_block
+from models.blocks import create_res_conv_block, create_input_layer, conv_block, factorized_bilinear_pooling_new, factorized_bilinear_pooling
 from models.rangeview_branch import create_range_view_branch
 
 def create_sensor_fusion_net():
-    bev_in = create_input_layer((700, 800, 10), 'bev')
+    bev_in = create_input_layer((448, 512, 32), 'bev')
     x = bev_in
 
     '''
         Backbone
     '''
     for i in range(2): # same res
-        x = create_res_conv_block(x, 24, 3)
+        x = conv_block(x, 64, 3, 1)
     f1 = x
         
     for i in range(4): # 1 / 2 res
-        x = create_res_conv_block(x, 48, 3, i==0)
+        x = create_res_conv_block(x, 128, 3, i==0)
     f2 = x
 
     for i in range(6): # 1 / 4 res
-        x = create_res_conv_block(x, 96, 3, i==0)
+        x = create_res_conv_block(x, 192, 3, i==0)
     f3 = x
 
     for i in range(6): # 1 / 8 res
-        x = create_res_conv_block(x, 192, 3, i==0)
+        x = create_res_conv_block(x, 256, 3, i==0)
     f4 = x
-
-    # for i in range(4): # 1 / 16 res
-    #     x = create_res_conv_block(x, 256, 3, i==0)
-    # f5 = x
-
 
     '''
         Range View Net
@@ -44,86 +39,67 @@ def create_sensor_fusion_net():
     rv_inputs  = rv_net['inputs']
     rv_outputs = rv_net['outputs']
 
-    l2, l3, l4, l5 = rv_outputs
+    l2, l3, l4 = rv_outputs
 
     '''
         RV FPN
     '''
-    rv_l2_dw = tf.image.resize(l2, 
-                            (175, 200),
-                            method='nearest')
-    rv_l2 = conv_block(rv_l2_dw, 96, 3, 1)
 
-    rv_l3_zp = tf.image.resize(l3, 
-                              (175, 200),
-                              method='nearest')
-    rv_l3 = conv_block(rv_l3_zp, 96, 3, 1)
+    out_map_sz = (112, 128)
+    TOP_DOWN_PYRAMID_SIZE = 96
 
-    rv_l4_zp = tf.image.resize(l4, 
-                            (175, 200),
-                            method='nearest')
-    rv_l4 = conv_block(rv_l4_zp, 96, 3, 1)
+    L2 = conv_block(l2, TOP_DOWN_PYRAMID_SIZE, 1, 1)
+    L2 = tf.image.resize(L2, (94, 311), 'nearest')
+    L2 = conv_block(L2, TOP_DOWN_PYRAMID_SIZE, 3, 1)
 
-    rv_l5_up = tf.image.resize(l5, 
-                            (175, 200),
-                            method='nearest')
-    rv_l5 = conv_block(rv_l5_up, 96, 3, 1)
+    L3 = conv_block(l3, TOP_DOWN_PYRAMID_SIZE, 1, 1)
 
-    rv_out = Add()([rv_l2, rv_l3, rv_l4, rv_l5])
+    L4 = conv_block(l4, TOP_DOWN_PYRAMID_SIZE, 1, 1)
+    L4 = tf.image.resize(L4, (94, 311), 'nearest')
+    L4 = conv_block(L4, TOP_DOWN_PYRAMID_SIZE, 3, 1)
+
+    RV_OUT = Add()([L2, L3, L4])
+    RV_OUT = tf.image.resize(RV_OUT, out_map_sz, 'nearest')
+    RV_OUT = conv_block(RV_OUT, TOP_DOWN_PYRAMID_SIZE, 3, 1)
 
     # pprint([rv_l2, rv_l3, rv_l4, rv_l5])
 
     '''
         BEV FPN + Header
     '''
-    f2_dw = MaxPooling2D()(f2)
-    f2_dw = conv_block(f2_dw, 96, 1, 1)
-    f2_dw = Add()([f2_dw, rv_out])
-    f2_dw = conv_block(f2_dw, 96, 3, 1)
+    F2 = conv_block(f2, TOP_DOWN_PYRAMID_SIZE, 1, 1)
+    F2 = tf.image.resize(F2, out_map_sz, 'nearest')
+    F2 = conv_block(F2, TOP_DOWN_PYRAMID_SIZE, 3, 1)
+    F2 = factorized_bilinear_pooling_new(F2, RV_OUT, TOP_DOWN_PYRAMID_SIZE, TOP_DOWN_PYRAMID_SIZE * 2)
+    # F2 = conv_block(F2, TOP_DOWN_PYRAMID_SIZE, 3, 1)
 
-    f3_zp = f3
-    f3_zp = conv_block(f3_zp, 96, 1, 1)
-    f3_zp = Add()([f3_zp, rv_out])
-    f3_zp = conv_block(f3_zp, 96, 3, 1)
+    F3 = f3
+    F3 = conv_block(F3, TOP_DOWN_PYRAMID_SIZE, 1, 1)
+    F3 = factorized_bilinear_pooling_new(F3, RV_OUT, TOP_DOWN_PYRAMID_SIZE, TOP_DOWN_PYRAMID_SIZE * 2)
+    # F3 = conv_block(F3, TOP_DOWN_PYRAMID_SIZE, 3, 1)
 
-    f4_up = tf.image.resize(f4, 
-                            (175, 200),
-                            method='nearest')
-    f4_up = conv_block(f4_up, 96, 1, 1)                            
-    f4_up = Add()([f4_up, rv_out])
-    f4_up = conv_block(f4_up, 96, 3, 1)
+    F4 = conv_block(f4, TOP_DOWN_PYRAMID_SIZE, 1, 1)
+    F4 =  tf.image.resize(F4, out_map_sz, method='nearest')
+    F4 = conv_block(F4, TOP_DOWN_PYRAMID_SIZE, 3, 1)
+    F4 = factorized_bilinear_pooling_new(F4, RV_OUT, TOP_DOWN_PYRAMID_SIZE, TOP_DOWN_PYRAMID_SIZE * 2)
+    # F4 = conv_block(F4, TOP_DOWN_PYRAMID_SIZE, 3, 1)
 
-    # f5_up = tf.image.resize(f5, 
-    #                         (175, 200),
-    #                         method='nearest')
-    # f5_up = conv_block(f5_up, 96, 1, 1)                        
-    # f5_up = Add()([f5_up, rv_l5])
-    # f5_up = conv_block(f5_up, 96, 3, 1)
-
-    out = Add()([f2_dw, f3_zp, f4_up])
-    out = create_res_conv_block(out, 96, 3)
+    BEV_OUT = Add()([F2, F3, F4])
+    OUT = create_res_conv_block(BEV_OUT, 96, 3)
 
     obj_map = Conv2D(filters=1, 
                      kernel_size=1, 
                      padding='same', 
                      activation='sigmoid', 
                      name='obj_map', 
-                     kernel_initializer='glorot_normal')(out)
-    geo_map = Conv2D(filters=11, 
+                     kernel_initializer='glorot_normal')(OUT)
+    geo_map = Conv2D(filters=8, 
                      kernel_size=1, 
                      padding='same', 
                      activation=None, 
                      name='geo_map',
-                     kernel_initializer='glorot_normal')(out)
+                     kernel_initializer='glorot_normal')(OUT)
 
-    # pprint([f2, f3, f4, f5])
-    # print('-----------------')
-    # pprint([f2_dw, f3_zp, f4_up, f5_up])
-    # print('-----------------')
-    # pprint([out])
-    # print('-----------------')
-    # pprint([obj_map, geo_map])
-    # print('-----------------')
 
     model = Model([bev_in] + rv_inputs, [obj_map, geo_map])
     # print(model.summary())
