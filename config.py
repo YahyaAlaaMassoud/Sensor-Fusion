ed2cada758f2ff1f48880445bf02da61f24ff9f8
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2020-08-16 23:56:08 -0400

    update configuration file for training

--- config.py
+++ config.py
@@ -28,13 +28,13 @@ from tensorflow.keras.layers import Activation
 
 configs = {
     'dataset_path': '/home/salam/datasets/KITTI/training', # absolute path
-    'gpu_id': '0', # zero-indexed (1-car | 0-ped) (7-car | 0-ped)
+    'gpu_id': '1', # zero-indexed (1-car | 0-ped) (7-car | 0-ped)
     'phy_width': 70,
     'phy_height': 80,
     'phy_depth': 4,
     'input_shape': (800, 700, 10),
-    'target_encoder': PIXORTargets(shape=(128, 112), stats=dd.io.load('kitti_stats/stats.h5'),
-                                    P_WIDTH=70, P_HEIGHT=80, P_DEPTH=4, subsampling_factor=(0.75, 1.25)),
+    'target_encoder': PIXORTargets(shape=(128, 112), stats=dd.io.load('kitti_stats/new_stats.h5'),
+                                    P_WIDTH=70, P_HEIGHT=80, P_DEPTH=4, subsampling_factor=(0.8, 1.2)),
     'pc_encoder': BEVVoxelizer(n_x=448, n_y=512, n_z=32, side_range=(0,70), fwd_range=(-40,40), height_range=(-1, 3), filter_external=True),#OccupancyCuboidKITTI(x_min=0, x_max=70, y_min=-40, y_max=40, z_min=-1, z_max=3, df=[0.1, 0.1, 0.4], densify=False),
     'target_shape': (128, 112),
     'target_means': np.array([-6.4501972e-03, 
@@ -52,11 +52,11 @@ configs = {
     'mean_height': 1.52,
     'mean_altitude': 1.71,
     'stats': dd.io.load('kitti_stats/stats.h5'),
-    'ckpts_dir': 'sensor_fusion_1',
+    'ckpts_dir': 'contfuse_mlb',
     'training_target': CARS_ONLY, # one of the enums {ALL_VEHICLES, CARS_ONLY, PEDESTRIANS_ONLY, CYCLIST_ONLY}
     'model_fn': create_sensor_fusion_net,
     'losses': {
-        'obj_map': focal(alpha=0.25, gamma=2., subsampling_flag=True, data_format='channels_last'),
+        'obj_map': focal(alpha=0.75, gamma=1.0, subsampling_flag=True, data_format='channels_last'),
         'geo_map': smooth_l1_loss(sigma=3., reg_channels=8, data_format='channels_last'),
     },
     'metrics': {
@@ -69,26 +69,27 @@ configs = {
                     reg_metric(5, 8),
                     reg_metric(6, 8),
                     reg_metric(7, 8),
-                    # reg_metric(8, 11),
+                    # reg_metric(8, 9),
                     # reg_metric(9, 11),
                     # reg_metric(10, 11),
                    ],
     },
     'callbacks': [],
     'optimizer': RMSprop,
-    'experiment_name': '/sensor_fusion_1',
-    'start_epoch': 0,
-    'use_pretrained': False,
-    'last_ckpt_json': 'outputs/car_efficient_det/car_efficient_det_epoch_59.json', # absolute path
-    'last_ckpt_h5': 'outputs/car_efficient_det/car_efficient_det_epoch_59.h5', # absolute path
+    'experiment_name': '/contfuse_mlb',
+    'start_epoch': 32,
+    'use_pretrained': True,
+    'last_ckpt_json': 'outputs/contfuse_mlb/logs/08-15-05-17-(1)/contfuse_mlb_epoch_31.json', # absolute path
+    'last_ckpt_h5': 'outputs/contfuse_mlb/logs/08-15-05-17-(1)/contfuse_mlb_epoch_31.h5', # absolute path
     'custom_objects': {'BiFPN': BiFPN, 'PriorProbability': PriorProbability, 'Swish': Swish, 'GroupNormalization': GroupNormalization}, # Dict as {'BiFPN': BiFPN}
     'current_file_name': __file__,
+    'current_model_file_name': '/home/yahyaalaa/Yahya/kitti_dev/models/sensor_fusion_net.py',
     'stable': 0.0001,
-    'schedule1': const_lr(1e-2), # CosineDecay(initial_learning_rate=1e-8, decay_steps=1800, alpha=1e6),
-    'schedule2': const_lr(1e-3), #CosineDecay(initial_learning_rate=1e-3, decay_steps=20000, alpha=0.1),
-    'warmup_steps': 6 * 30,
+    'schedule1': const_lr(1e-4), # CosineDecay(initial_learning_rate=1e-8, decay_steps=1800, alpha=1e6),
+    'schedule2': const_lr(1e-4), # CosineDecay(initial_learning_rate=1e-3, decay_steps=20000, alpha=0.1),
+    'warmup_steps': 1856 * 1,
     'data_format': 'channels_last',
-    'vis_every': 30, # 12691
+    'vis_every': 1856 // 2, # 12691
 
     'hyperparams': {
         'batch_size': 2,
@@ -97,7 +98,7 @@ configs = {
         'num_threads': 4,
         'max_q_size': 2,
         'validate_every': 1000,
-        'ckpt_every': 100000,
+        'ckpt_every': 1,
         'n_val_samples': 300,
         'map_every': 5000,
     },
@@ -113,16 +114,14 @@ configs = {
         '000058',
         '000059',
         '000061',
-        # '000062',
-        # '000063',
-        # '000065',
-        # '000066',
-        # '000076',
+        '000062',
+        '000063',
+        '000065',
+        '000066',
+        '000076',
     ],
 
     'notes': '''
-        without random sampling augmentation
-        with 0.5 change random augmentation selected
-        weighted BiFPN
+        feature map fusion -> Add
     '''
 }
